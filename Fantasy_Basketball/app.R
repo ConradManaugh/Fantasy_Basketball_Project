0a69b87b016657ba49914fd05f7a85a4d141bdc7
Anne Janulis <ajanuli2@illinois.edu>
2018-05-04 22:33:43 +0000

    Completion of the first shiny main panel tab with all specialty datasets

--- Fantasy_Basketball/app.R
+++ Fantasy_Basketball/app.R
@@ -7,9 +7,18 @@
 #    http://shiny.rstudio.com/
 #
 
+library(dplyr)
 library(shiny)
 library(magrittr)
+all_data = read.csv("all_data.csv")
 pg_data = read.csv("pg_data.csv")
+c_data = read.csv("c_data.csv")
+f_data = read.csv("f_data.csv")
+gf_data = read.csv("gf_data.csv")
+pf_data = read.csv("pf_data.csv")
+psg_data = read.csv("psg_data.csv")
+sf_data = read.csv("sf_data.csv")
+sg_data = read.csv("sg_data.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -22,7 +31,7 @@ ui <- fluidPage(
         sidebarPanel(
             selectInput("position",
                         label = h5("Position"),
-                        choices = list("Point Guard",
+                        choices = c("Point Guard",
                         "Point / Shooting Guard",
                         "Shooting Guard",
                         "Forward",
@@ -31,13 +40,10 @@ ui <- fluidPage(
                         "Power Forward",
                         "Center",
                         "All Positions")),
-            numericInput("fg_weight",
-                         label = h5("Weight of FG"),
-                         value = 1),
-            numericInput("3pt_weight",
+            numericInput("three_weight",
                          label = h5("Weight of 3PT"),
                          value = 1), 
-            numericInput("2pt_weight",
+            numericInput("two_weight",
                          label = h5("Weight of 2PT"),
                          value = 1),
             numericInput("ft_weight",
@@ -81,20 +87,117 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
-    active_dataset = reactive({
+    total_data = reactive({
         if(input$position == "Point Guard"){
-            pg_data
+            total = input$three_weight * pg_data$pred_Bhind_Arc + 
+                input$two_weight * pg_data$pred_In_Arc + input$ft_weight * pg_data$pred_FreeT +
+                input$ofr_weight * pg_data$pred_OfReb + input$dfr_weight * pg_data$pred_DefReb + 
+                input$as_weight * pg_data$pred_Assist + input$st_weight * pg_data$pred_Steal +
+                input$bl_weight * pg_data$pred_Block + input$tov_weight * pg_data$pred_TOV +
+                input$fo_weight * pg_data$pred_Fouls
         }
-        else{
+        else if(input$position == "Point / Shooting Guard"){
+            total = input$three_weight * psg_data$pred_Bhind_Arc + 
+                input$two_weight * psg_data$pred_In_Arc + input$ft_weight * psg_data$pred_FreeT +
+                input$ofr_weight * psg_data$pred_OfReb + input$dfr_weight * psg_data$pred_DefReb + 
+                input$as_weight * psg_data$pred_Assist + input$st_weight * psg_data$pred_Steal + 
+                input$bl_weight * psg_data$pred_Block + input$tov_weight * psg_data$pred_TOV +
+                input$fo_weight * psg_data$pred_Fouls   
+        }
+        else if(input$position == "Shooting Guard"){
+            total = input$three_weight * sg_data$pred_Bhind_Arc + 
+                input$two_weight * sg_data$pred_In_Arc + input$ft_weight * sg_data$pred_FreeT +
+                input$ofr_weight * sg_data$pred_OfReb + input$dfr_weight * sg_data$pred_DefReb + 
+                input$as_weight * sg_data$pred_Assist + input$st_weight * sg_data$pred_Steal + 
+                input$bl_weight * sg_data$pred_Block + input$tov_weight * sg_data$pred_TOV +
+                input$fo_weight * sg_data$pred_Fouls  
             
         }
+        else if(input$position == "Forward"){
+            total = input$three_weight * f_data$pred_Bhind_Arc + 
+                input$two_weight * f_data$pred_In_Arc + input$ft_weight * f_data$pred_FreeT +
+                input$ofr_weight * f_data$pred_OfReb + input$dfr_weight * f_data$pred_DefReb + 
+                input$as_weight * f_data$pred_Assist + input$st_weight * f_data$pred_Steal + 
+                input$bl_weight * f_data$pred_Block + input$tov_weight * f_data$pred_TOV +
+                input$fo_weight * f_data$pred_Fouls  
+        }
+        else if(input$position == "Guard / Forward"){
+            total = input$three_weight * gf_data$pred_Bhind_Arc + 
+                input$two_weight * gf_data$pred_In_Arc + input$ft_weight * gf_data$pred_FreeT +
+                input$ofr_weight * gf_data$pred_OfReb + input$dfr_weight * gf_data$pred_DefReb + 
+                input$as_weight * gf_data$pred_Assist + input$st_weight * gf_data$pred_Steal + 
+                input$bl_weight * gf_data$pred_Block + input$tov_weight * gf_data$pred_TOV +
+                input$fo_weight * gf_data$pred_Fouls  
+        }
+        else if(input$position == "Small Forward"){
+            total = input$three_weight * sf_data$pred_Bhind_Arc + 
+                input$two_weight * sf_data$pred_In_Arc + input$ft_weight * sf_data$pred_FreeT +
+                input$ofr_weight * sf_data$pred_OfReb + input$dfr_weight * sf_data$pred_DefReb + 
+                input$as_weight * sf_data$pred_Assist + input$st_weight * sf_data$pred_Steal + 
+                input$bl_weight * sf_data$pred_Block + input$tov_weight * sf_data$pred_TOV +
+                input$fo_weight * sf_data$pred_Fouls  
+        }
+        else if(input$position == "Power Forward"){
+            total = input$three_weight * pf_data$pred_Bhind_Arc + 
+                input$two_weight * pf_data$pred_In_Arc + input$ft_weight * pf_data$pred_FreeT +
+                input$ofr_weight * pf_data$pred_OfReb + input$dfr_weight * pf_data$pred_DefReb + 
+                input$as_weight * pf_data$pred_Assist + input$st_weight * pf_data$pred_Steal + 
+                input$bl_weight * pf_data$pred_Block + input$tov_weight * pf_data$pred_TOV +
+                input$fo_weight * pf_data$pred_Fouls 
+        }
+        else if(input$position == "Center"){
+            total = input$three_weight * c_data$pred_Bhind_Arc + 
+                input$two_weight * c_data$pred_In_Arc + input$ft_weight * c_data$pred_FreeT +
+                input$ofr_weight * c_data$pred_OfReb + input$dfr_weight * c_data$pred_DefReb + 
+                input$as_weight * c_data$pred_Assist + input$st_weight * c_data$pred_Steal + 
+                input$bl_weight * c_data$pred_Block + input$tov_weight * c_data$pred_TOV +
+                input$fo_weight * c_data$pred_Fouls
+        }
+        else if(input$position == "All Positions"){
+            total = input$three_weight * all_data$pred_Bhind_Arc + 
+                input$two_weight * all_data$pred_In_Arc + input$ft_weight * all_data$pred_FreeT +
+                input$ofr_weight * all_data$pred_OfReb + input$dfr_weight * all_data$pred_DefReb + 
+                input$as_weight * all_data$pred_Assist + input$st_weight * all_data$pred_Steal + 
+                input$bl_weight * all_data$pred_Block + input$tov_weight * all_data$pred_TOV +
+                input$fo_weight * all_data$pred_Fouls
+        }
     })
-    finalized_data = reactive({
-        total =  3pt_weight*pg_data$pred_Bhind_Arc + 2pt_weight*
+    active_dataset = reactive({
+        if(input$position == "Point Guard"){
+            data.frame("Player" = pg_data$Name,"Predicted_Total_Points" = total_data())
+        }
+        else if(input$position == "Point / Shooting Guard"){
+            data.frame("Player" = psg_data$Name,"Predicted_Total_Points" = total_data())
+        }
+        else if(input$position == "Shooting Guard"){
+            data.frame("Player" = sg_data$Name,"Predicted_Total_Points" = total_data())
+        }
+        else if(input$position == "Forward"){
+            data.frame("Player" = f_data$Name,"Predicted_Total_Points" = total_data())
+        }
+        else if(input$position == "Guard / Forward"){
+            data.frame("Player" = gf_data$Name,"Predicted_Total_Points" = total_data())
+        }
+        else if(input$position == "Small Forward"){
+            data.frame("Player" = sf_data$Name,"Predicted_Total_Points" = total_data())
+        }
+        else if(input$position == "Power Forward"){
+            data.frame("Player" = pf_data$Name,"Predicted_Total_Points" = total_data())
+        }
+        else if(input$position == "Center"){
+            data.frame("Player" = c_data$Name,"Predicted_Total_Points" = total_data())
+        }
+        else if(input$position == "All Positions"){
+            data.frame("Player" = all_data$Name,"Predicted_Total_Points" = total_data())
+        }
+    })
+    
+    more_dataset = reactive({
+        top_n(active_dataset(), input$obs, Predicted_Total_Points)
     })
     
     output$table = renderTable({
-        head(active_dataset(), n = input$obs)
+        more_dataset()
     })
 }
 
